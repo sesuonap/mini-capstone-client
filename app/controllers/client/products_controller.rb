6b3fcee4a473b0618e8f7e16e3e93228aa8f11cc
sesuonap <tpanouses@gmail.com>
2018-05-14 21:30:57 -0500

    added rest of routes

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -3,5 +3,57 @@ class Client::ProductsController < ApplicationController
     response = Unirest.get("http://localhost:3000/api/products")
     @products = response.body
     render 'index.html.erb'
+  end
+  
+  def new
+    render 'new.html.erb'
   end 
+
+  def create
+    client_params = {
+                      name: params[:name],
+                      price: params[:price],
+                      image_url: params[:image_url],
+                      description: params[:description],
+                    }
+    response = Unirest.post("http://localhost:3000/api/products",
+                              parameters: client_params)
+    render 'create.html.erb'
+  end
+
+  def show
+     product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
+    @product = response.body
+    render 'show.html.erb'
+  end 
+
+  def edit
+    product_id = params[:id] 
+    response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
+    @product = response.body
+    render 'edit.html.erb'
+
+  end 
+
+  def update
+    client_params = {
+                      name: params[:name],
+                      price: params[:price],
+                      image_url: params[:image_url],
+                      description: params[:description],
+                    }
+    response = Unirest.patch("http://localhost:3000/api/products/#{  params[:id] }",
+      parameters: client_params)
+
+    render 'update.html.erb'
+
+  end
+
+  def destroy
+    product_id = params[:id]
+    response = Unirest.delete("http://localhost:3000/api/products/#{product_id}")
+    render 'destroy.html.erb'
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get '/' => 'client/products#index'
   namespace :client do 
     get '/products' => 'products#index'
+    get '/products/new' => 'products#new'
+    post '/products' => 'products#create'
+    get '/products/:id' => 'products#show'
+    get '/products/:id/edit' => 'products#edit'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
   end 
 end
