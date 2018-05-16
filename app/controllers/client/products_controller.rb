8f1df88290bb331fbf5a1d1426ded4e5659603ef
sesuonap <tpanouses@gmail.com>
2018-05-15 21:32:02 -0500

    5/18

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -18,7 +18,9 @@ class Client::ProductsController < ApplicationController
                     }
     response = Unirest.post("http://localhost:3000/api/products",
                               parameters: client_params)
-    render 'create.html.erb'
+    product = response.body
+    flash[:success] = "Successfully Updated Item"
+    redirect_to "/client/products/#{ product["id"] }"
   end
 
   def show
@@ -46,14 +48,17 @@ class Client::ProductsController < ApplicationController
     response = Unirest.patch("http://localhost:3000/api/products/#{  params[:id] }",
       parameters: client_params)
 
-    render 'update.html.erb'
+    product = response.body
+    flash[:success] = "Successfully Updated Item"
+    redirect_to "/client/products/#{ product["id"] }"
 
   end
 
   def destroy
     product_id = params[:id]
     response = Unirest.delete("http://localhost:3000/api/products/#{product_id}")
-    render 'destroy.html.erb'
+    
+    redirect_to "/"
   end  
 
 end
